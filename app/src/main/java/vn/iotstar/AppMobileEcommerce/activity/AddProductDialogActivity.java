128640f1234ef9546c1b3849f41e5e58a42821bd
Thanh Tin <letin210303@gmail.com>
2024-05-19 12:36:48 +0700

    thanh toan ngan hang

---
+++ app/src/main/java/vn/iotstar/AppMobileEcommerce/activity/AddProductDialogActivity.java
@@ -0,0 +1,187 @@
+package vn.iotstar.AppMobileEcommerce.activity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import vn.iotstar.AppMobileEcommerce.R;
+import vn.iotstar.AppMobileEcommerce.api.ProductAPI;
+import vn.iotstar.AppMobileEcommerce.model.dto.ResponseObject;
+import vn.iotstar.AppMobileEcommerce.retrofit.RetrofitClient;
+import vn.iotstar.AppMobileEcommerce.service.RealPathUtil;
+
+public class AddProductDialogActivity extends AppCompatActivity {
+
+    private Button btnSelectImage, btnAddProduct, btnCancel;
+    private ImageView imgView;
+    private EditText edtProductName;
+    private EditText edtDescri;
+    private EditText edtBranId;
+    private Uri imgUrl;
+    private Uri mUri;
+    private static final int REQUEST_CODE_READ_EXTERNAL_STORAGE = 9;
+    private static final int PICK_IMAGE_REQUEST = 10;
+    private ProductAPI productAPI;
+    static int id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestReadExternalStoragePermission();
+
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_add_product);
+
+        productAPI = RetrofitClient.getRetrofit().create(ProductAPI.class);
+
+        imgView = findViewById(R.id.selected_image);
+
+        btnSelectImage = findViewById(R.id.btnSelect_);
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+        edtProductName = findViewById(R.id.edtName_);
+        edtDescri = findViewById(R.id.edtDescri);
+        edtBranId = findViewById(R.id.edtBrandId);
+
+        btnCancel = findViewById(R.id.btnCancel_);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnAddProduct = findViewById(R.id.btnSave_);
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addProduct();
+            }
+        });
+
+        Intent intent = getIntent();
+        id = intent.getIntExtra("idBrand", 0);
+        edtBranId.setText(String.valueOf(id));
+        edtBranId.setEnabled(false);
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imgUrl = data.getData();
+            mUri = imgUrl;
+            String realPath = RealPathUtil.getRealPath(this, mUri);
+            if (realPath != null) {
+                Picasso.get().load(imgUrl).into(imgView);
+            } else {
+                Toast.makeText(this, "Cannot get real path of image", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void addProduct() {
+        if (edtProductName.getText().toString().trim().isEmpty()) {
+            Toast.makeText(AddProductDialogActivity.this, "Please enter product name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+//        if (imgUrl == null) {
+//            Toast.makeText(AddProductDialogActivity.this, "Please select product image", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+        if (edtBranId == null) {
+            Toast.makeText(AddProductDialogActivity.this, "Please enter brand of product", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        RequestBody productName = RequestBody.create(MediaType.parse("multipart/form-data"), edtProductName.getText().toString().trim());
+        //Thêm mô tả
+        RequestBody productDescri = RequestBody.create(MediaType.parse("multipart/form-data"), edtDescri.getText().toString().trim());
+        //Thêm brand
+        RequestBody brandId = RequestBody.create(MediaType.parse("multipart/form-data"), String.valueOf(id));
+
+        File file = new File(RealPathUtil.getRealPath(this, imgUrl));
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part image = MultipartBody.Part.createFormData("images", file.getName(), requestFile);
+
+        Call<ResponseObject> call = productAPI.insert(productName, productDescri, brandId ,image);
+        call.enqueue(new Callback<ResponseObject>() {
+            @Override
+            public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                if (response.isSuccessful()) {
+                    ResponseObject result = response.body();
+                    if (result != null && result.getStatus().equals("Success")) {
+                        Toast.makeText(AddProductDialogActivity.this, "Product added successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(AddProductDialogActivity.this, "Failed to add product: " + result.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(AddProductDialogActivity.this, "Failed to add product", Toast.LENGTH_SHORT).show();
+                }
+                startActivity(new Intent(AddProductDialogActivity.this, AdminPanelBrandActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseObject> call, Throwable t) {
+                Toast.makeText(AddProductDialogActivity.this, "Failed to call API"+ t.getMessage(), Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(AddProductDialogActivity.this, AdminPanelBrandActivity.class));
+                finish();
+            }
+        });
+    }
+    private void requestReadExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CODE_READ_EXTERNAL_STORAGE);
+        } else {
+            // Quyền đã được cấp, thực hiện các hành động tương ứng
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Quyền được cấp, thực hiện các hành động tương ứng
+                } else {
+                    // Quyền không được cấp, thông báo cho người dùng biết
+                    Toast.makeText(this, "Read external storage permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/vn/iotstar/AppMobileEcommerce/activity/AdminProductGridActivity.java
@@ -0,0 +1,134 @@
+package vn.iotstar.AppMobileEcommerce.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import vn.iotstar.AppMobileEcommerce.R;
+import vn.iotstar.AppMobileEcommerce.adapter.RecycleAdapterProductGrid;
+import vn.iotstar.AppMobileEcommerce.api.ProductAPI;
+import vn.iotstar.AppMobileEcommerce.model.ProductGridModel;
+import vn.iotstar.AppMobileEcommerce.retrofit.RetrofitClient;
+
+public class AdminProductGridActivity extends AppCompatActivity {
+    private List<ProductGridModel> productGridModelList;
+    private RecycleAdapterProductGrid mAdapter2;
+    private RecyclerView recyclerview;
+    ImageView iv_back;
+    TextView title;
+    ProductAPI productAPI;
+    static int id;
+    EditText inputSearch;
+    Button btn_add;
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_product_admin);
+        anhXa();
+        this.title.setText("Product Grid");
+        Intent intent = getIntent();
+        id = intent.getIntExtra("idBrand", 0);
+        AdminProductGridActivity();
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminProductGridActivity.this.getBaseContext(), AddProductDialogActivity.class);
+                intent.putExtra("idBrand", id);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+        //Search products by name
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String searchQuery = s.toString();
+                // Gọi phương thức tìm kiếm sản phẩm với searchQuery
+                searchProduct(searchQuery);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        iv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminProductGridActivity.this, AdminPanelHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void searchProduct(String searchQuery) {
+        // Tìm kiếm sản phẩm theo tên
+        List<ProductGridModel> productList = getProductListByName(searchQuery);
+
+        // Cập nhật danh sách sản phẩm trong RecyclerView
+        mAdapter2.setProductList(productList);
+    }
+    private List<ProductGridModel> getProductListByName(String searchQuery) {
+        List<ProductGridModel> productList = new ArrayList<>();
+        for (ProductGridModel product : productGridModelList) {
+            if (product.getProductName().toLowerCase().contains(searchQuery.toLowerCase())) {
+                productList.add(product);
+            }
+        }
+        return productList;
+    }
+    @SuppressLint("NotConstructor")
+    public void AdminProductGridActivity() {
+        productAPI= RetrofitClient.getRetrofit().create(ProductAPI.class);
+        productAPI.getProductByBrand(id).enqueue(new Callback<List<ProductGridModel>>() {
+            @Override
+            public void onResponse(Call<List<ProductGridModel>> call, Response<List<ProductGridModel>> response) {
+                if(response.isSuccessful()){
+                    productGridModelList = response.body();
+                    mAdapter2 = new RecycleAdapterProductGrid(productGridModelList, AdminProductGridActivity.this);
+                    recyclerview.setLayoutManager(new GridLayoutManager(AdminProductGridActivity.this, 2));
+                    recyclerview.setItemAnimator(new DefaultItemAnimator());
+                    recyclerview.setAdapter(mAdapter2);
+                }else{
+                    int statusCode = response.code();
+                }
+            }
+            @Override
+            public void onFailure(Call<List<ProductGridModel>> call, Throwable t) {
+            }
+        });
+    }
+    void anhXa(){
+        recyclerview = findViewById(R.id.recyclerview_product_grid);
+        title = findViewById(R.id.title);
+        iv_back = findViewById(R.id.iv_back);
+        inputSearch = findViewById(R.id.search_editext);
+        btn_add = findViewById((R.id.btn_add));
+    }
+}
